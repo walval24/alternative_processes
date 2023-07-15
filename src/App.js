0aa0fe7befffb6a7244cf7bc0ee98b5d8c882eca
Walter Valentini <walval@outlook.it>
2023-07-15 19:14:54 +0200

    update react router dom

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Switch>
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          {/* <Route path='/sign-up' component={SignUp} /> */}
           <Route path='/' exact component={Home} />
         </Switch>
       </Router>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,59 +23,59 @@ function Footer() {
         </div>
       </section>
       
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
             <Link to='/' className='social-logo'>
               Alternative Processes
             </Link>
           </div>
-          <small class='website-rights'>AP © 2023</small>
-          <div class='social-icons'>
+          <small className='website-rights'>AP © 2023</small>
+          <div className='social-icons'>
             <a
-              class='social-icon-link facebook'
+              className='social-icon-link facebook'
               href='https://www.facebook.com/groups/780786319000246'
               target='_blank'
               rel='noopener noreferrer'
               aria-label='Facebook'
             >
-              <i class='fab fa-facebook-f' />
+              <i className='fab fa-facebook-f' />
             </a>
             <a
-              class='social-icon-link instagram'
+              className='social-icon-link instagram'
               href='https://www.instagram.com/alternativeprocesses/'
               target='_blank'
               rel='noopener noreferrer'
               aria-label='Instagram'
             >
-              <i class='fab fa-instagram' />
+              <i className='fab fa-instagram' />
             </a>
             <a
-              class='social-icon-link youtube'
+              className='social-icon-link youtube'
               href='https://www.youtube.com/channel/UCoGvr0lDqpt141tYjq4KZPg'
               target='_blank'
               rel='noopener noreferrer'
               aria-label='Youtube'
             >
-              <i class='fab fa-youtube' />
+              <i className='fab fa-youtube' />
             </a>
             <a
-              class='social-icon-link tiktok'
+              className='social-icon-link tiktok'
               href='https://www.tiktok.com/login?redirect_url=https%3A%2F%2Fwww.tiktok.com%2F&lang=en&enter_method=mandatory'
               target='_blank'
               rel='noopener noreferrer'
               aria-label='TikTok'
             >
-              <i class='fab fa-tiktok' />
+              <i className='fab fa-tiktok' />
             </a>
             <a
-              class='social-icon-link linkedin'
+              className='social-icon-link linkedin'
               href='https://www.linkedin.com/company/alternative-processes/'
               target='_blank'
               rel='noopener noreferrer'
               aria-label='LinkedIn'
             >
-              <i class='fab fa-linkedin' />
+              <i className='fab fa-linkedin' />
             </a>
           </div>
         </div>
